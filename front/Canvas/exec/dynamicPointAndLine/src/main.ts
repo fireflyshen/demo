fe7b70a1e6e24d6236de9987d1ab1b7d3ffaf3f1
firefly <you@example.com>
2025-03-31 10:49:04 +0800

    123

--- front/Canvas/exec/dynamicPointAndLine/src/main.ts
+++ front/Canvas/exec/dynamicPointAndLine/src/main.ts
@@ -3,8 +3,7 @@ import { CustomAnimation } from ".";
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /* html */ `
   <canvas id="canvas"></canvas>
 `
-
-const htmlCanvas = document.querySelector<HTMLCanvasElement>("#canvas") ;
+const htmlCanvas = document.querySelector<HTMLCanvasElement>("#canvas");
 // 启动动画
 if (htmlCanvas) {
   const animation = new CustomAnimation(htmlCanvas, 20);
